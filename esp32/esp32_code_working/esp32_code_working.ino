ca425624ed513792a48cf25ca064630259a4a89a
flash-275 <vanshmalani27@gmail.com>
2025-04-23 15:56:52 +0530

    hehe

--- esp32/esp32_code_working/esp32_code_working.ino
+++ esp32/esp32_code_working/esp32_code_working.ino
@@ -96,6 +96,27 @@ class CommandCallback : public BLECharacteristicCallbacks {
   }
 };
 
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    pServer->updateConnParams(pServer->getConnId(), 12, 12, 0, 100); // Optional: Adjust connection parameters
+    pServer->getAdvertising()->stop(); // Stop advertising when connected
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    pServer->getAdvertising()->start(); // Restart advertising when disconnected
+  }
+};
+
+void sendNotification(String data) {
+  const int chunkSize = 20; // Adjust based on MTU size
+  for (size_t i = 0; i < data.length(); i += chunkSize) {
+    String chunk = data.substring(i, i + chunkSize);
+    pCharacteristic->setValue(chunk.c_str());
+    pCharacteristic->notify();
+    delay(10); // Small delay to ensure the client processes the notification
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -121,11 +142,13 @@ void setup() {
 
   BLEDevice::init("ESP32-GATT-Manager");
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   pCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_WRITE
+    BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE
   );
   pCharacteristic->setCallbacks(new CommandCallback());
 
@@ -134,6 +157,9 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->start();
+
+  // Request a larger MTU size
+  BLEDevice::setMTU(512); // Maximum MTU size supported by BLE
 
   Serial.println("BLE GATT running. Waiting for command...");
 }
